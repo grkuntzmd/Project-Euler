54ca76786a34e06bfad251699d76bd6e70ef4fce
G. Ralph Kuntz, MD <grk@usa.net>
2014-07-16 12:06:12 +0000

    Problem 25

---
+++ src/Problem25.hs
@@ -0,0 +1,8 @@
+module Main where
+
+-- | The main entry point.
+main :: IO ()
+main = print $ fst $ head $ dropWhile (\(n, f) ->
+        length (show f) < 1000) $ zip [1..] fibs
+  where
+    fibs = 1 : 1 : zipWith (+) fibs (tail fibs)
